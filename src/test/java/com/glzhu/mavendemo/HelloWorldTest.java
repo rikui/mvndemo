efc8d2c2e1d56a2eca5a38f96fcdea42cbb08399
glzhu <glzhu@niub.la>
2016-11-14 14:57:55 +0800

    change something

--- src/test/java/com/glzhu/mavendemo/HelloWorldTest.java
+++ src/test/java/com/glzhu/mavendemo/HelloWorldTest.java
@@ -6,14 +6,19 @@ import org.junit.Test;
 public class HelloWorldTest {
 
 	@Test
-	public void testMethod(){
+	public void testMethod() {
 		HelloWorld hw = new HelloWorld();
-			int ss= hw.add(1, 1);
-			Assert.assertEquals(ss, 2);
-			
-			int ss2= hw.add(1, 2);
-			Assert.assertEquals(ss2, 3);
+		int ss = hw.add(1, 1);
+		Assert.assertEquals(ss, 2);
+
 	}
-	
-	
+
+	@Test
+	public void testMethod1() {
+		HelloWorld hw = new HelloWorld();
+
+		int ss2 = hw.add(1, 2);
+		Assert.assertEquals(ss2, 3);
+	}
+
 }
